e2de81d73e2db8a87850e9704630c1b4e8e00642
thomaswilliamscode <64794727+thomaswilliamscode@users.noreply.github.com>
2026-05-14 16:27:02 +0000

    init commit

---
+++ script.js
@@ -0,0 +1,79 @@
+// Problem 1
+// Find the indices of two numbers that add up to target
+
+const nums1 = [2, 7, 11, 15];
+const target1 = 9;
+
+function twoSum(nums, target) {
+
+}
+
+console.log(twoSum(nums1, target1));
+
+// Problem 2
+// Array is already sorted
+// Return indices of two numbers that add up to target
+
+const nums2 = [1, 2, 4, 6, 10];
+const target2 = 8;
+
+function twoSumSorted(nums, target) {
+
+}
+
+console.log(twoSumSorted(nums2, target2));
+
+// Problem 3
+// Find the sum closest to target
+
+const nums3 = [-1, 2, 1, -4];
+const target3 = 4;
+
+function twoSumClosest(nums, target) {
+
+}
+
+console.log(twoSumClosest(nums3, target3));
+
+
+// Problem 4
+// Return all unique pairs that add up to target
+
+const nums4 = [1, 2, 3, 2, 4, 0, 5];
+const target4 = 5;
+
+function allPairs(nums, target) {
+
+}
+
+console.log(allPairs(nums4, target4));
+
+
+// Problem 5
+// Design a class with:
+// add(number)
+// find(target)
+
+class TwoSum {
+
+    constructor() {
+
+    }
+
+    add(number) {
+
+    }
+
+    find(target) {
+
+    }
+}
+
+const ts = new TwoSum();
+
+ts.add(1);
+ts.add(3);
+ts.add(5);
+
+console.log(ts.find(4));
+console.log(ts.find(7));
