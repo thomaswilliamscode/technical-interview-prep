4a73a42fcf46257e89b0190f94e7627f974d4045
thomaswilliamscode <64794727+thomaswilliamscode@users.noreply.github.com>
2026-06-09 16:54:04 +0000

    test

--- script.js
+++ script.js
@@ -1,98 +1,314 @@
 
 
-/*
-Problem:
-Reverse only letters, keep non-letters in place.
+// // --------------------------------------------------
+// // 3. Intersection of Two Sorted Arrays
+// // --------------------------------------------------
 
-Example:
-"a-bC-d" → "d-Cb-a"
-*/
+// /*
+// Problem:
+// Given two sorted arrays,
+// return all common elements.
 
-function reverseOnlyLetters(s) {
+// Example:
+
+// nums1 = [1,2,2,3,4]
+// nums2 = [2,2,4,6]
+
+// Output:
+// [2,2,4]
+// */
+
+function intersectSorted(nums1, nums2) {
     // TODO
+    // add same numbers to output array 
+    let result = [];
+
+    // pointers
+    let nums1Index = 0;
+    let nums2Index = 0; 
+
+    
+    
+    // check less than or equal to.
+
 }
 
-/*
-Problem:
-Reverse order of words.
+let nums1 = [1,2,2,3,4];
+let nums2 = [2,2,4,6];
 
-Example:
-"the sky is blue" → "blue is sky the"
-*/
+intersectSorted(nums1, nums2);
 
-function reverseWords(s) {
-    // TODO
-}
+// // --------------------------------------------------
+// // 4. Union of Two Sorted Arrays
+// // --------------------------------------------------
 
-/*
-Problem:
-Reverse each word individually.
+// /*
+// Problem:
+// Return a sorted array containing
+// all unique elements from both arrays.
 
-Example:
-"hello world" → "olleh dlrow"
-*/
+// Example:
 
-function reverseEachWord(s) {
-    // TODO
-}
+// nums1 = [1,2,4]
+// nums2 = [2,3,5]
 
-/*
-Problem:
-Reverse only vowels in a string.
+// Output:
+// [1,2,3,4,5]
+// */
 
-Example:
-"leetcode" → "leotcede"
-*/
+// function unionSorted(nums1, nums2) {
+//     // TODO
+// }
 
-function reverseVowels(s) {
-    // TODO
-}
+// let nums1 = [1,2,4];
+// let nums2 = [2,3,5];
 
-/*
-Problem:
-Reverse the prefix of a string up to first occurrence of a given character.
+// unionSorted(nums1, nums2);
 
-Example:
-word = "abcdefd", ch = "d"
-→ "dcbaefd"
-*/
+// // --------------------------------------------------
+// // 5. Merge Intervals
+// // --------------------------------------------------
 
-function reversePrefix(word, ch) {
-    // TODO
-}
+// /*
+// Problem:
+// Given an array of intervals,
+// merge all overlapping intervals.
 
-/*
-Problem:
-Reverse a string using recursion.
+// Example:
 
-Example:
-"hello" → "olleh"
-*/
+// [[1,3],[2,6],[8,10],[15,18]]
 
-function reverseRecursion(s) {
-    // TODO
-}
+// Output:
 
-/*
-Problem:
-Reverse first k characters of a string.
+// [[1,6],[8,10],[15,18]]
+// */
 
-Example:
-"abcdef", k = 3 → "cbadef"
-*/
+// function mergeIntervals(intervals) {
+//     // TODO
+// }
 
-function reverseFirstK(s, k) {
-    // TODO
-}
+// let intervals = [
+//     [1,3],
+//     [2,6],
+//     [8,10],
+//     [15,18]
+// ];
+
+// mergeIntervals(intervals);
+
+
+//------------------------------------------------------------------------------------------
+
+// --------------------------------------------------
+// 2. Contains Duplicate
+// --------------------------------------------------
 
 /*
 Problem:
-Reverse only letters, keep digits unchanged in position.
+Return true if any value appears at least twice.
 
 Example:
-"a1b2c3" → "c1b2a3"
+[1,2,3,1]
+
+Output:
+true
 */
 
-function reverseLettersKeepDigits(s) {
-    // TODO
-}
+// function containsDuplicate(nums) {
+//     // TODO
+// }
+
+// let nums = [1,2,3,1];
+
+// containsDuplicate(nums);
+
+// // --------------------------------------------------
+// // 3. Valid Anagram
+// // --------------------------------------------------
+
+// /*
+// Problem:
+// Return true if t is an anagram of s.
+
+// Example:
+// s = "anagram"
+// t = "nagaram"
+
+// Output:
+// true
+// */
+
+// function isAnagram(s, t) {
+//     // TODO
+// }
+
+// let s = "anagram";
+// let t = "nagaram";
+
+// isAnagram(s, t);
+
+// // --------------------------------------------------
+// // 4. First Unique Character
+// // --------------------------------------------------
+
+// /*
+// Problem:
+// Return the index of the first non-repeating character.
+
+// Example:
+// "leetcode"
+
+// Output:
+// 0
+// */
+
+// function firstUniqueChar(s) {
+//     // TODO
+// }
+
+// let str = "leetcode";
+
+// firstUniqueChar(str);
+
+// // --------------------------------------------------
+// // 5. Find Maximum Consecutive Ones
+// // --------------------------------------------------
+
+// /*
+// Problem:
+// Find the maximum number of consecutive 1s.
+
+// Example:
+// [1,1,0,1,1,1]
+
+// Output:
+// 3
+// */
+
+// function findMaxConsecutiveOnes(nums) {
+//     // TODO
+// }
+
+// let nums = [1,1,0,1,1,1];
+
+// findMaxConsecutiveOnes(nums);
+
+// // --------------------------------------------------
+// // 6. Length of Last Word
+// // --------------------------------------------------
+
+// /*
+// Problem:
+// Return the length of the last word.
+
+// Example:
+// "Hello World"
+
+// Output:
+// 5
+// */
+
+// function lengthOfLastWord(s) {
+//     // TODO
+// }
+
+// let sentence = "Hello World";
+
+// lengthOfLastWord(sentence);
+
+// // --------------------------------------------------
+// // 7. Reverse Vowels of a String
+// // --------------------------------------------------
+
+// /*
+// Problem:
+// Reverse only the vowels.
+
+// Example:
+// "hello"
+
+// Output:
+// "holle"
+// */
+
+// function reverseVowels(s) {
+//     // TODO
+// }
+
+// let word = "hello";
+
+// reverseVowels(word);
+
+// // --------------------------------------------------
+// // 8. Intersection of Two Arrays
+// // --------------------------------------------------
+
+// /*
+// Problem:
+// Return unique values that appear in both arrays.
+
+// Example:
+// nums1 = [1,2,2,1]
+// nums2 = [2,2]
+
+// Output:
+// [2]
+// */
+
+// function intersection(nums1, nums2) {
+//     // TODO
+// }
+
+// let nums1 = [1,2,2,1];
+// let nums2 = [2,2];
+
+// intersection(nums1, nums2);
+
+// // --------------------------------------------------
+// // 9. Majority Element
+// // --------------------------------------------------
+
+// /*
+// Problem:
+// Return the element that appears more than n/2 times.
+
+// Example:
+// [3,2,3]
+
+// Output:
+// 3
+// */
+
+// function majorityElement(nums) {
+//     // TODO
+// }
+
+// let nums = [3,2,3];
+
+// majorityElement(nums);
+
+// // --------------------------------------------------
+// // 10. Best Time to Buy and Sell Stock
+// // --------------------------------------------------
+
+// /*
+// Problem:
+// Return the maximum profit possible.
+
+// Example:
+// [7,1,5,3,6,4]
+
+// Output:
+// 5
+
+// Buy at 1
+// Sell at 6
+// */
+
+// function maxProfit(prices) {
+//     // TODO
+// }
+
+// let prices = [7,1,5,3,6,4];
+
+// maxProfit(prices);
